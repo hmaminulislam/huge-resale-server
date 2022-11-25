fc25bc3966167fb0f2f68cde6af08ab09acbf624
Aminul <mdaminulislamsahin14@gmail.com>
2022-11-25 07:42:32 +0600

    category api added

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000
 
 //midleware
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.wpflsxi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try{
+        const productsCollection = client.db("HugeResale").collection("products");
+
+        app.get('/category/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {brand: id}
+            const result = await productsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
+
+run().catch(console.log);
 
 app.get('/', (req, res) => {
     res.send('Huge Resale server is running...')
