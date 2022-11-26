aaee462e9333f3cf2c8cc61e96ce17905b17ce81
Aminul <mdaminulislamsahin14@gmail.com>
2022-11-26 14:29:36 +0600

    user delete api added

--- index.js
+++ index.js
@@ -103,6 +103,14 @@ async function run() {
       );
       res.send(result);
     });
+
+    app.delete('/users/:email', async(req, res) => {
+        const email = req.params.email;
+        const query = {email: email}
+        const result = await usersCollection.deleteOne(query)
+        res.send(result)
+    })
+    
   } catch (error) {
     console.log(error);
   }
